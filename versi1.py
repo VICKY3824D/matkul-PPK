79486d0d472cdb26f6adbb77805cc974b7fe24c3
Vidky <pradana2766@gmail.com>
2024-09-20 14:42:36 +0700

    Menghapus Obat dan Menjual obat

--- versi1.py
+++ versi1.py
@@ -15,17 +15,21 @@ while True:
         daftar_obat = [nama_obat,harga_obat,stok_obat,kadaluarsa]
         obat.append(daftar_obat)
         print(f"obat {nama_obat}  berhasil ditambahkan dengan stok {stok_obat}")
+    
+    #menambah obat
     elif menu == 2:
         if len(obat) > 0:
-            for i in obat: 
-                print("========") 
-                print("Nama Obat: ", daftar_obat[0]) 
-                print("Harga Obat: ", daftar_obat[1]) 
-                print("Stok Obat: ", daftar_obat[2]) 
-                print("Tanggal Kadaluarsa: ", daftar_obat[3]) 
+            for i, daftar_obat in enumerate(obat):
+                print(f"Obat {i+1}:")
+                print(f"  Nama: {daftar_obat[0]}")
+                print(f"  Harga: {daftar_obat[1]}")
+                print(f"  Stok: {daftar_obat[2]}")
+                print(f"  Tanggal Kadaluarsa: {daftar_obat[3]}")
                 print("========")  
         else:
             print("Belum ada obat yang ditambahkan.")
+            
+    #Menghapus obat
     elif menu == 3:
         hapus = input("Hapus obat mana : ").lower()
         for j in obat:
@@ -33,15 +37,15 @@ while True:
                 obat.remove(j)
                 print(f"obat {hapus} telah berhasi dihapus")
     
-    # #Menjual obat     
-    # elif menu == 4:         
-    #     jual = input("Jual obat: ").lower()    
-    #     jumlah = int(input("jumlah: ")) 
-    #     for j in obat:            
-    #         if jual in j:                 
-    #             stok_obat = stok_obat - jumlah                
-    #             print(f"{jual} terjual sebanyak {jumlah} unit. sisa stok: {stok_obat}")  #bug: stok tidak berkurang     
+    #Menjual obat     
+    elif menu == 4:         
+        jual = input("Jual obat: ").lower()    
+        jumlah = int(input("jumlah: ")) 
+        for j in obat:            
+            if jual in j:                 
+                j[2] = j[2] - jumlah                
+                print(f"{jual} terjual sebanyak {jumlah} unit. sisa stok: {j[2]}")  #bug: stok tidak berkurang     
                                    
-    #     total = jumlah *  harga_obat         
-    #     print(f"struk: {jual}, jumlah: {jumlah}, total : {total}") 
+        total = jumlah *  harga_obat         
+        print(f"struk: {jual}, jumlah: {jumlah}, total : {total}") 
     
