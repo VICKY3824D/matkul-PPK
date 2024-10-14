40592afa0010cf04185989b056bb150e45c33581
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-10-14 09:00:40 +0700

    menghapus country

--- API.py
+++ API.py
@@ -9,14 +9,13 @@ n = int(input("n: "))
 for i in range(n):
     # Parameter yang akan dikirimkan ke API, misalnya nama acara TV
     title = input("Tite: ")
-    negara = input("country: ")
     params = {
         "q": title, # Ganti dengan nama acara TV yang ingin dicari
-        "country": negara
     }
 
     # Mengirim permintaan GET ke API
     response = requests.get(api_url, params=params)
+    print(response.url)
 
     # Memeriksa apakah permintaan berhasil
     if response.status_code == 200:
@@ -28,7 +27,6 @@ for i in range(n):
         print(f"Deskripsi: {show_data['summary'].strip('<p>').strip('</p>')}")
         print(f"Status: {show_data['status']}")
         print(f"Tanggal Premier: {show_data['premiered']}")
-        print(f"Negara dari params: {params['country']}")
         print(f"Skor: {show_data['rating']['average']}")
         print(f"Link: {show_data['officialSite']}")
         print("--------------------")
