2efedf1bddd229c1a46e0a620b39eeaecda48e9e
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-10-14 19:52:03 +0700

    memisahkan dict

--- API.py
+++ API.py
@@ -15,21 +15,33 @@ for i in range(n):
 
     # Mengirim permintaan GET ke API
     response = requests.get(api_url, params=params)
-    print(response.url)
+    print(response.url);
 
     # Memeriksa apakah permintaan berhasil
     if response.status_code == 200:
         # Mengambil data JSON dari hasil API
-        show_data = response.json()
-        print(show_data)
+        show_data = response.json();
+        for i in show_data:
+            if type(show_data[i]) ==  dict:
+                print(i, "==> ", end=' ');
+                for j in show_data[i]:
+                    print(f'{j}\t: {show_data[i][j]}, ',end="");
+            else:
+                print(f'{i}\t\t: {show_data[i]}')
+        # print(show_data);
+        
+        # jjsn = json.dumps(show_data); #convert  dictionary ke json
+        # print(jjsn);
+        # print(type(jjsn));
+        
         # Menampilkan informasi acara TV
-        print(f"Judul Acara: {params['q']}")
-        print(f"Deskripsi: {show_data['summary'].strip('<p>').strip('</p>')}")
-        print(f"Status: {show_data['status']}")
-        print(f"Tanggal Premier: {show_data['premiered']}")
-        print(f"Skor: {show_data['rating']['average']}")
-        print(f"Link: {show_data['officialSite']}")
-        print("--------------------")
+        # print("--------------------")
+        # print(f"Judul Acara: {params['q']}")
+        # print(f"Deskripsi: {show_data['summary'].strip('<p>').strip('</p>')}")
+        # print(f"Status: {show_data['status']}")
+        # print(f"Tanggal Premier: {show_data['premiered']}")
+        # print(f"Skor: {show_data['rating']['average']}")
+        # print(f"Link: {show_data['officialSite']}")
     else:
         print(f"Terjadi kesalahan: {response.status_code}")
         print("--------------------")
